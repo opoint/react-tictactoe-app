fec432d15c70639966b7e2bb7bd877a2e8f228ef
윤영규 <yyk@naver.com>
2024-01-17 12:58:02 +0900

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Square from './Square';
+import "./Board.css";
+
+export default class Board extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="status">Next Player: X, O</div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+        </div>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+

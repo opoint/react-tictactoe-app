56bed6eec9b4ebbf0e44ede1e24c7ac4da6a7cbf
윤영규 <yyk@naver.com>
2024-01-17 17:53:57 +0900

    완성 전

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,49 +1,34 @@
-import React, { Component } from 'react';
 import Square from './Square';
 import "./Board.css";
 
-export default class Board extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
-    }
-  }
-
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]} 
-      onClick={() => {this.handleClick(i)}}
+const Board = ({squares, onClick}) => {
+  
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} 
+      onClick={() => {onClick(i)}}
     />
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="status">Next Player: X, O</div>
-        <div className='board-row'>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className='board-row'>
-        </div>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        <div className='board-row'>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    )
-  }
-}
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
 
+};
+
+export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import "./Square.css";
 
-export default class Square extends React.Component {
+const Square = ({onClick, value}) => {
 
-  render() {
-    return(
-      <button className='square'
-        onClick={() => { 
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    )
-  }
+  return(
+    <button className='square' onClick={onClick}>
+      {value}
+    </button>
+  )
+
 }
+
+export default Square;
